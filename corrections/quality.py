b464ef415a0bdb6eeff66b86cdc9f357803a862c
Rasmus Handberg <rasmush@phys.au.dk>
2018-12-08 18:36:06 +0100

    Cleanup in quality.py

--- corrections/quality.py
+++ corrections/quality.py
@@ -8,9 +8,51 @@ Handling of TESS data quality flags.
 
 from __future__ import division, with_statement, print_function, absolute_import
 
-class CorrectorQualityFlags(object):
-	"""
+#------------------------------------------------------------------------------
+class QualityFlagsBase(object):
 
+	@classmethod
+	def decode(cls, quality):
+		"""
+		Converts a QUALITY value into a list of human-readable strings.
+		This function takes the QUALITY bitstring that can be found for each
+		cadence in TESS data files and converts into a list of human-readable
+		strings explaining the flags raised (if any).
+
+		Parameters:
+			quality (int): Value from the 'QUALITY' column of a TESS data file.
+
+		Returns:
+			list of str: List of human-readable strings giving a short
+			             description of the quality flags raised.
+						 Returns an empty list if no flags raised.
+		"""
+		result = []
+		for flag in cls.STRINGS.keys():
+			if quality & flag != 0:
+				result.append(cls.STRINGS[flag])
+		return result
+
+	@classmethod
+	def filter(cls, quality, flags=None):
+		"""
+		Filter quality flags against a specific set of flags.
+
+		Parameters:
+			quality (integer or ndarray): Quality flags.
+			flags (integer bitmask): Default=``TESSQualityFlags.DEFAULT_BITMASK``.
+
+		Returns:
+			ndarray: ``True`` if quality DOES NOT contain any of the specified ``flags``, ``False`` otherwise.
+
+		"""
+		if flags is None: flags = cls.DEFAULT_BITMASK
+		return (quality & flags == 0)
+
+#------------------------------------------------------------------------------
+class CorrectorQualityFlags(QualityFlagsBase):
+	"""
+	This class encodes the meaning of the various TESS QUALITY bitmask flags.
 	"""
 	FlaggedBadData = 1
 	ManualExclude = 2
@@ -24,9 +66,22 @@ class CorrectorQualityFlags(object):
 	# Default bitmask
 	DEFAULT_BITMASK = (FlaggedBadData | ManualExclude)
 
-class TESSQualityFlags(object):
+	# Pretty string descriptions for each flag
+	STRINGS = {
+		FlaggedBadData: "Bad data based on pixel flags",
+		ManualExclude: "Manual exclude",
+		SigmaClip: "Point removed due to sigma clipping",
+		JumpAdditiveConstant: "Jump corrected using additive constant",
+		JumpAdditiveLinear: "Jump corrected using additive linear trend",
+		JumpMultiplicativeConstant: "Jumb corrected using multiplicative constant",
+		JumpMultiplicativeLinear: "Jump corrected using multiplicative linear trend",
+		Interpolated: "Point is interpolated"
+	}
+
+#------------------------------------------------------------------------------
+class TESSQualityFlags(QualityFlagsBase):
 	"""
-	This class encodes the meaning of the various TESS QUALITY bitmask flags.
+	This class encodes the meaning of the various TESS PIXEL_QUALITY bitmask flags.
 	"""
 	AttitudeTweak = 1
 	SafeMode = 2
@@ -54,53 +109,16 @@ class TESSQualityFlags(object):
 
 	# Pretty string descriptions for each flag
 	STRINGS = {
-		1: "Attitude tweak",
-		2: "Safe mode",
-		4: "Spacecraft in Coarse point",
-		8: "Spacecraft in Earth point",
-		16: "Reaction wheel zero crossing",
-		32: "Reaction wheel desaturation event",
-		64: "Cosmic ray in optimal aperture pixel",
-		128: "Manual exclude",
-		256: "Sudden sensitivity dropout",
-		512: "Impulsive outlier",
-		1024: "Cosmic ray in collateral data",
-		2048: "Earth, Moon or other planet in camera FOV"
+		AttitudeTweak: "Attitude tweak",
+		SafeMode: "Safe mode",
+		CoarsePoint: "Spacecraft in Coarse point",
+		EarthPoint: "Spacecraft in Earth point",
+		ZeroCrossing: "Reaction wheel zero crossing",
+		Desat: "Reaction wheel desaturation event",
+		ApertureCosmic: "Cosmic ray in optimal aperture pixel",
+		ManualExclude: "Manual exclude",
+		SensitivityDropout: "Sudden sensitivity dropout",
+		ImpulsiveOutlier: "Impulsive outlier",
+		CollateralCosmic: "Cosmic ray in collateral data",
+		EarthMoonPlanetInFOV: "Earth, Moon or other planet in camera FOV"
 	}
-
-	@classmethod
-	def decode(cls, quality):
-		"""
-		Converts a TESS QUALITY value into a list of human-readable strings.
-		This function takes the QUALITY bitstring that can be found for each
-		cadence in TESS data files and converts into a list of human-readable
-		strings explaining the flags raised (if any).
-
-		Parameters:
-			quality (int): Value from the 'QUALITY' column of a TESS data file.
-
-		Returns:
-			list of str: List of human-readable strings giving a short
-			             description of the quality flags raised.
-						 Returns an empty list if no flags raised.
-		"""
-		result = []
-		for flag in cls.STRINGS.keys():
-			if quality & flag > 0:
-				result.append(cls.STRINGS[flag])
-		return result
-
-	@staticmethod
-	def filter(quality, flags=DEFAULT_BITMASK):
-		"""
-		Filter quality flags against a specific set of flags.
-
-		Parameters:
-			quality (integer or ndarray): Quality flags.
-			flags (integer bitmask): Default=``TESSQualityFlags.DEFAULT_BITMASK``.
-
-		Returns:
-			ndarray: ``True`` if quality DOES NOT contain any of the specified ``flags``, ``False`` otherwise.
-
-		"""
-		return (quality & flags == 0)
