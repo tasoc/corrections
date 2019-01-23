04c24059a736d5a0baef7a342458fe42334d0cc5
Oliver Hall <ojh251@student.bham.ac.uk>
2019-01-23 16:28:03 +0100

    Wrote the most basic unit test.

--- tests/test_ensemble.py
+++ tests/test_ensemble.py
@@ -10,11 +10,33 @@ from __future__ import division, print_function, with_statement, absolute_import
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from corrections import ensemble
+from corrections import EnsembleCorrector
 
 INPUT_DIR = os.path.join(os.path.dirname(__file__), 'input')
 
+starid = '??'
+camera = '??'
+ccd = '??'
 #----------------------------------------------------------------------
-def test_basic():
-	assert 3 = 1+2
+def test_ensemble_basics():
+	"""Check that the Ensemblecorrector can be initiated at all"""
+	with EnsembleCorrector(INPUT_DIR, plot=True) as ec:
+		assert ec.__class__.__name__ == 'EnsembleCorrector', "Did not get the correct class name back"
+		assert ec.input_folder == INPUT_DIR, "Incorrect input folder"
+		assert ec.plot == True, "Plot parameter passed appropriately"
+
+def test_ensemble_correction():
+	"""Check that ensemble performs its most basic correction."""
+	CorrClass = corrections.corrclass('ensemble')
+	#Initiate the class
+	with CorrClass(INPUT_DIR, plot=False) as corr:
+		#Create a task
+		with corrections.TaskManager(INPUT_DIR) as tm:
+			task = tm.get_task(starid=starid, camera=camera, ccd=ccd)
+		#Read in the data
+		inlc = corr.load_lightcurve(task)
+
+		outlc, status = corr.do_correction(inlc)
+	return inlc, outlc, status
+
 #----------------------------------------------------------------------
