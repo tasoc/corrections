f4238d364f10a23a79de4bff3436ab55a7f3534a
Oliver Hall <ojh251@student.bham.ac.uk>
2019-01-24 17:26:30 +0100

    Added unit tests. Fails due to bug in main code.

--- tests/test_ensemble.py
+++ tests/test_ensemble.py
@@ -10,55 +10,63 @@ from __future__ import division, print_function, with_statement, absolute_import
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from corrections import EnsembleCorrector
+import corrections
 
-INPUT_DIR = os.path.join(os.path.dirname(__file__), 'input')
+# INPUT_DIR = os.path.join(os.path.dirname(__file__), 'input')
+INPUT_DIR = '../../TESS_data/lightcurves-2127753/'
 
-starid = '??'
-camera = '??'
-ccd = '??'
+# TODO: Build a dummy structure that we can test failures on
+# TODO: Check the frange and etc algebraically
+starid = 281703991
+camera = 3
+ccd = 1
+sector =1
 #----------------------------------------------------------------------
 def test_ensemble_basics():
 	"""Check that the Ensemblecorrector can be initiated at all"""
-	with EnsembleCorrector(INPUT_DIR, plot=True) as ec:
+	with corrections.EnsembleCorrector(INPUT_DIR, plot=True) as ec:
 		assert ec.__class__.__name__ == 'EnsembleCorrector', "Did not get the correct class name back"
 		assert ec.input_folder == INPUT_DIR, "Incorrect input folder"
 		assert ec.plot == True, "Plot parameter passed appropriately"
 
-def test_ensemble_correction():
-	"""Check that ensemble performs its most basic correction."""
-	CorrClass = corrections.corrclass('ensemble')
-	#Initiate the class
-	with CorrClass(INPUT_DIR, plot=False) as corr:
-		#Create a task
-		with corrections.TaskManager(INPUT_DIR) as tm:
-			task = tm.get_task(starid=starid, camera=camera, ccd=ccd)
-		#Read in the data
-		inlc = corr.load_lightcurve(task)
-
-		outlc, status = corr.do_correction(inlc)
-	return inlc, outlc, status
 
 def test_ensemble_return_values():
 	""" Check that the ensemble returns values that are reasonable and within expected bounds """
-	CorrClass = corrections.corrclass('ensemble')
+	tm = corrections.TaskManager(INPUT_DIR)
+	task = tm.get_task(starid=starid, camera=camera, ccd=ccd)
+
 	#Initiate the class
-	with CorrClass(INPUT_DIR, plot=False) as corr:
-		#Create a task
-		with corrections.TaskManager(INPUT_DIR) as tm:
-			task = tm.get_task(starid=starid, camera=camera, ccd=ccd)
-		#Read in the data
-		inlc = corr.load_lightcurve(task)
+	CorrClass = corrections.corrclass('ensemble')
+	corr = CorrClass(INPUT_DIR, plot=False)
+	inlc = corr.load_lightcurve(task)
+	outlc, status = corr.do_correction(inlc.copy())
 
-		outlc, status = corr.do_correction(inlc)
+	#Check contents
+	assert len(outlc.flux) == len(inlc.flux), "Input flux ix different length to output flux"
+	assert all(inlc.time == outlc.time), "Input time is nonidentical to output time"
+	assert all(outlc.flux != inlc.flux), "Input and output flux are identical."
+	assert len(outlc.flux) == len(outlc.time), "Check time and flux have same length"
 
-		assert status == STATUS.OK, "STATUS was not set appropriately"		
-		assert outlc.meta[]
-		assert outlc.meta[]
-		assert outlc.meta[]
-		assert outlc.meta[]
-	
+	#Check metadata
+	assert 'fmean' in outlc.meta, "Metadata is incomplete"
+	assert 'fstd' in outlc.meta, "Metadata is incomplete"
+	assert 'frange' in outlc.meta, "Metadata is incomplete"
+	assert 'drange' in outlc.meta, "Metadata is incomplete"
+	assert outlc.meta['task']['starid'] == inlc.meta['task']['starid'], "Metadata is incomplete"
+	assert outlc.meta.task == inlc.meta.task, "Metadata is incomplete"
 
-#----------------------------------------------------------------------
-if if __name__ == "__main__":
-	pass # placeholder
+	#Check status
+	assert status == corrections.STATUS.OK, "STATUS was not set appropriately"
+
+def test_ensemble_cbv_comparison():
+	pass
+
+if __name__ == "__main__":
+	tm = corrections.TaskManager(INPUT_DIR)
+	task = tm.get_task(starid=starid, camera=camera, ccd=ccd)
+
+	#Initiate the class
+	CorrClass = corrections.corrclass('ensemble')
+	corr = CorrClass(INPUT_DIR, plot=False)
+	inlc = corr.load_lightcurve(task)
+	outlc, status = corr.do_correction(inlc.copy())
