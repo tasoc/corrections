1b7d216349360baab8bdd48affbd278d96884f28
Lindsey Carboneau <lmcarboneau@users.noreply.github.com>
2019-03-20 13:26:30 -0400

    Updates to ensemble algorithm, debugging options

--- corrections/ensemble.py
+++ corrections/ensemble.py
@@ -126,7 +126,7 @@ class EnsembleCorrector(BaseCorrector):
         # List of star indexes to be included in the ensemble
         temp_list = []
         # Initial number of closest stars to consider and variable to increase number
-        initial_num_stars = 10
+        initial_num_stars = 4
         star_count = initial_num_stars
         # (Alternate param) Initial distance at which to consider stars around the target
         # initial_search_radius = -1
@@ -196,13 +196,13 @@ class EnsembleCorrector(BaseCorrector):
                     # 2 sigma
                     ens2sig = 2 * np.std(mens_flux)
                     targ2sig = 2 * np.std(mtarget_flux)
-                    
-                    # absolute balue
+                    # absolute value
                     abstarg = np.absolute(mtarget_flux)
                     absens = np.absolute(mens_flux)
 
-                    logger.info("2 sigma")
-                    logger.info(str(ens2sig) + " , " + str(targ2sig))
+                    if debug:
+                        logger.info("2 sigma")
+                        logger.info(str(ens2sig) + " , " + str(targ2sig))
 
                     # sigma clip the flux used to fit, but don't use that flux again
                     clip_target_flux = np.where(
