2328673dc7798c5c6757072b2c1305ce0a612149
Mikkel Nørup Lund <mikkelnl@phys.au.dk>
2019-03-21 13:33:23 +0100

    Adding spike sep

--- run_cbvprep.py
+++ run_cbvprep.py
@@ -23,8 +23,9 @@ def prepare_cbv(cbv_area, input_folder=None, threshold=None, ncbv=None, el=None,
 
 	with CBVCorrector(input_folder, threshold_snrtest=threshold, ncomponents=ncbv) as C:
 		C.compute_cbvs(cbv_area, ent_limit=el)
-#		C.cotrend_ini(cbv_area, do_ini_plots=ip)
-#		C.compute_distance_map(cbv_area)
+		C.spike_sep(cbv_area)
+		C.cotrend_ini(cbv_area, do_ini_plots=ip)
+		C.compute_distance_map(cbv_area)
 
 #------------------------------------------------------------------------------
 if __name__ == '__main__':
