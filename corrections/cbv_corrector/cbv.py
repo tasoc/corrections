37bec94d2510eaabd19e3238689ddfb04f71b79d
Rasmus Handberg <rasmush@phys.au.dk>
2020-06-15 10:59:00 +0200

    Catch freak ValueError in CBV fit

--- corrections/cbv_corrector/cbv.py
+++ corrections/cbv_corrector/cbv.py
@@ -121,7 +121,7 @@ class CBV(object):
 #		C = (np.linalg.inv(X.T.dot(X)).dot(X.T)).dot(F)
 		try:
 			C = (np.linalg.pinv(X.T.dot(X)).dot(X.T)).dot(F)
-		except np.linalg.LinAlgError:
+		except (np.linalg.LinAlgError, ValueError):
 			# Another (but slover) implementation
 			C = np.linalg.lstsq(X, F)[0]
 
